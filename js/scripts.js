407aed4013795783495a6a039e20f99b4edf896b
Nakul Gulati <nakgulati@gmail.com>
2016-02-20 15:16:35 +0530

    modular, js magic

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+function loadContent() {
+    var hash = $(location).attr("hash");
+
+    if ($("#navbar").hasClass("in")) {
+        $('.collapse').collapse('hide');
+    }
+
+    switch (hash) {
+        case "#gsoc":
+            $("#page-content").load("gsoc.html");
+            break;
+        case ("#projects"):
+            $("#page-content").load("projects.html");
+            break;
+        case "#resume":
+            $("#page-content").load("resume.html");
+            break;
+        case "#contact":
+            $("#page-content").load("contact.html");
+            break;
+        default:
+            $("#page-content").load("intro.html");
+            break;
+    }
+}
+
+function setup() {
+
+    $(window).on('hashchange', function () {
+        loadContent();
+    });
+
+    loadContent();
+}
